98dd754c5eab22ee629cf4385153a9ce8cfd6f50
Najaf Ali <ali.najaf@gmail.com>
2011-08-04 15:59:16 -0400

    got volume control working

--- xmonad.hs
+++ xmonad.hs
@@ -1,7 +1,14 @@
 --
 -- Basic xmonad config
 --
+-- on ubuntu, you'll need to run:
+--
+-- $ sudo apt-get install happy cabal-install
+-- $ cabal-install xmonad-extras
+--
+
 import XMonad
+import XMonad.Actions.Volume
 import XMonad.Util.EZConfig(additionalKeys)
 
 main = xmonad $ defaultConfig
@@ -11,4 +18,7 @@ main = xmonad $ defaultConfig
     , modMask = mod4Mask 
     } `additionalKeys`
     [ ((mod4Mask .|. shiftMask, xK_l), spawn "gnome-screensaver-command -l")
+    , ((mod4Mask, xK_Down), lowerVolume 10 >> return ())
+    , ((mod4Mask, xK_Up),   raiseVolume 10 >> return ())
+    , ((mod4Mask, xK_m),    toggleMute     >> return ())
     ]
