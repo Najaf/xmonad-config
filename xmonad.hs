02f105da546feeb7b520e6a91f1620aee7abb7ae
Najaf Ali <ali.najaf@gmail.com>
2011-08-04 16:10:35 -0400

    stuff for mobar

--- xmonad.hs
+++ xmonad.hs
@@ -3,19 +3,29 @@
 --
 -- on ubuntu, you'll need to run:
 --
--- $ sudo apt-get install happy cabal-install
+-- $ sudo apt-get install xmobar happy cabal-install
 -- $ cabal-install xmonad-extras
 --
 
 import XMonad
 import XMonad.Actions.Volume
 import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import System.IO
 
-main = xmonad $ defaultConfig
+main = do 
+  xmproc <- spawnPipe "/usr/bin/xmobar ~/.xmonad/.xmobarrc"
+  xmonad $ defaultConfig
     { borderWidth = 2
     , normalBorderColor = "#cccccc"
     , focusedBorderColor = "#cd8b00" 
     , modMask = mod4Mask 
+    , logHook = dynamicLogWithPP xmobarPP
+                        { ppOutput = hPutStrLn xmproc
+                        , ppTitle = xmobarColor "green" "" . shorten 50
+                        }
     } `additionalKeys`
     [ ((mod4Mask .|. shiftMask, xK_l), spawn "gnome-screensaver-command -l")
     , ((mod4Mask, xK_Down), lowerVolume 10 >> return ())
